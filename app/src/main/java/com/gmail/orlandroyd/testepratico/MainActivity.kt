aaabb18d07893ac2d80e870d04c7baad88430d1b
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-05-24 13:22:53 -0400

    login navigation

--- app/src/main/java/com/gmail/orlandroyd/testepratico/MainActivity.kt
+++ app/src/main/java/com/gmail/orlandroyd/testepratico/MainActivity.kt
@@ -2,16 +2,17 @@ package com.gmail.orlandroyd.testepratico
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.drawerlayout.widget.DrawerLayout.LOCK_MODE_LOCKED_CLOSED
+import androidx.drawerlayout.widget.DrawerLayout.LOCK_MODE_UNLOCKED
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.gmail.orlandroyd.testepratico.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,22 +29,30 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
+
+        // Navigation
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Setup login screen
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.loginFragment) {
+                supportActionBar!!.hide()
+                drawerLayout.setDrawerLockMode(LOCK_MODE_LOCKED_CLOSED)
+            } else {
+                supportActionBar!!.show()
+                drawerLayout.setDrawerLockMode(LOCK_MODE_UNLOCKED)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/gmail/orlandroyd/testepratico/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/gmail/orlandroyd/testepratico/presentation/login/LoginFragment.kt
@@ -1,12 +1,10 @@
 package com.gmail.orlandroyd.testepratico.presentation.login
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.gmail.orlandroyd.testepratico.R
-import com.gmail.orlandroyd.testepratico.databinding.FragmentHomeBinding
 import com.gmail.orlandroyd.testepratico.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -20,6 +18,11 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
         // init binding
         _binding = FragmentLoginBinding.bind(view)
+
+        binding.btnLogin.isEnabled = true
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToNavHome())
+        }
 
     }
 
